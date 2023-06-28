2d3dda88a4dc4327eb55638a2591f81f6e05b0ad
theroos <roosonline7@gmail.com>
2023-06-28 18:17:50 +0530

    Speech to text phase 2

--- app/src/main/java/com/example/theroos/speechtotext/MainActivity.kt
+++ app/src/main/java/com/example/theroos/speechtotext/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.example.theroos.speechtotext
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import java.util.*
 
 private val RQ_SPEECH_REC = 102
 
@@ -18,7 +26,26 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RQ_SPEECH_REC && resultCode == Activity.RESULT_OK)
+        {
+            val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            val txtext = findViewById<TextView>(R.id.tv_test)
+            txtext.text = result?.get(0).toString()
+        }
+    }
+
     private fun speechtotext() {
-        TODO("Not yet implemented")
+        if(!SpeechRecognizer.isRecognitionAvailable(this)){
+            Toast.makeText(this,"Speech Recognition is not available",Toast.LENGTH_LONG).show()
+        } else {
+            val i = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+            i.putExtra(RecognizerIntent.EXTRA_PROMPT,"Say Something")
+            startActivityForResult(i, RQ_SPEECH_REC)
+        }
     }
 }
